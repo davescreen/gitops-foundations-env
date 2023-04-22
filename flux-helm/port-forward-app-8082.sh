102df2c79b909fcab26fd56ffcf446c379df23bd
davescreen <davescreen@gmail.com>
2023-04-22 15:30:51 +0100

    Add index.yaml

--- flux-helm/port-forward-app-8082.sh
+++ flux-helm/port-forward-app-8082.sh
@@ -1,4 +1,4 @@
 # Flux installed in the 'flux' minikube cluster
 set -x
 # Or can minikube tunnel as the LoadBalancer starts port 80
-kubectl port-forward svc/flux-helm-app -n flux-helm 8082:80
+kubectl port-forward svc/my-chart -n my-chart-ns 8082:80
